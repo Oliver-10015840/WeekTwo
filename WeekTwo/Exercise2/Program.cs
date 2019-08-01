f67ff278abd1579feb9dc8c556ef164d1f243740
Oliver Irvin <10015840@student.toiohomai.ac.nz>
2019-08-01 13:09:45 +1200

    week2

---
+++ WeekTwo/Exercise2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the fruit: ");
+            string fruit = Console.ReadLine().ToLower();
+            Console.WriteLine("How many Kilograms would you like to purchase? ");
+            double kgs = double.Parse(Console.ReadLine());
+            double Price;
+            switch (fruit)
+            {
+                case "apple":
+                    Price = 1.25 * kgs;
+                    Console.WriteLine("Price for {0}kgs of Apples is ${1}",kgs,Price);
+                    break;
+                case "bannana":
+                    Price = 3.15 * kgs;
+                    Console.WriteLine("Price for {0}kgs of Apples is ${1}", kgs,Price);
+                    break;
+                case "kiwifuit":
+                    Price = 4.65 * kgs;
+                    Console.WriteLine("Price for {0}kgs of Apples is ${1}", kgs,Price);
+                    break;
+                case "oranges":
+                    Price = 2.75 * kgs;
+                    Console.WriteLine("Price for {0}kgs of Apples is ${1}", kgs,Price);
+                    break;
+                default:
+                    Console.WriteLine("Invalid!");
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ WeekTwo/WeekTwo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("Please enter a bird type: ");
+                string birdType = Console.ReadLine().ToLower();
+                switch (birdType)
+                {
+                    case "pigeon":
+                        Console.WriteLine("Correct!");
+                        loop = false;
+                        break;
+                    case "kiwi":
+                    case "seagull":
+                    case "parrot":
+                    case "chicken":
+                        Console.WriteLine("Incorrect!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid!");
+                        break;
+                }
+                Console.WriteLine("Try Again!");
+                Console.WriteLine();
+            }
+            Console.WriteLine("Game Over!");
+            }
+        }
+    }
